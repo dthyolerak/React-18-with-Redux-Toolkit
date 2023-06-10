470d536b24701aef1b044580b3827de64a4fde2b
McDonnex Kamsonga <thyolerak@gmail.com>
2023-06-10 17:58:32 +0200

    passing props and accessing props values

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import './index.css'
+const title = 'El Vert - unlock'
+const artist = 'Vert - unlock ft Co musiq'
+const img =
+  'https://mvaka.com/media/Malawi/profile1384267590/[mvaka]-El-Vert---unlock--[downloaded-on-mvaka-music-website].jpeg'
 
 const SongList = () => {
   return (
     <section className='song-list'>
       <h1 className='main-title'>List of new Songs</h1>
       <div className='songs-contain'>
-        <Song />
-        <Song />
-        <Song />
-        <Song />
-        <Song />
+        <Song title={title} artist={artist} img={img} />
+        <Song title={title} artist={artist} img={img} />
+        <Song title={title} artist={artist} img={img} />
+        <Song title={title} artist={artist} img={img} />
+        <Song title={title} artist={artist} img={img} />
       </div>
     </section>
   )
 }
 
-const Song = () => {
-  const title = 'El Vert - unlock'
-  const artist = 'Vert - unlock ft Co musiq'
-  const img = 'https://mvaka.com/media/Malawi/profile1384267590/[mvaka]-El-Vert---unlock--[downloaded-on-mvaka-music-website].jpeg'
+const Song = (props) => {
   return (
     <div className='song'>
-      <img
-        src={img}
-        alt={title}
-      />
-      <h2 style={{ color: 'red' }}> {title} </h2>
-      <p> {artist} </p>
+      <img src={props.img} alt={props.title} />
+      <h2 style={{ color: 'red' }}> {props.title} </h2>
+      <p> {props.artist} </p>
+      <p>{props.testing}</p>
     </div>
   )
 }
